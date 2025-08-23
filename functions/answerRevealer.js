9a6a635f8d613a2033d3854fcf6d372e0ea5c6df
Davizinzkn <117551326+davizinzknVkv@users.noreply.github.com>
2025-08-23 03:11:15 -0300

    Update answerRevealer.js

--- functions/answerRevealer.js
+++ functions/answerRevealer.js
@@ -15,7 +15,7 @@ JSON.parse = function (e, t) {
                                 widget.options.choices.forEach(choice => {
                                     if (choice.correct) {
                                         choice.content = "✅ " + choice.content;
-                                        sendToast("🔓 Respostas reveladas.", 1000);                
+                                        sendToast("🔓 Respostas Hackeada.", 1000);                
                                     }
                                 });
                             }
