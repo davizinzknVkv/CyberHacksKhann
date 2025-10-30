eea4b92619e6e7343b4a93e9ad0ea0841764a5b4
Davizinzkn <117551326+davizinzknVkv@users.noreply.github.com>
2025-10-30 20:33:37 -0300

    COR NEW

--- visuals/mainMenu.js
+++ visuals/mainMenu.js
@@ -70,7 +70,7 @@ Object.assign(watermark.style, {
 
 if (device.mobile) watermark.style.left = '65%'
 
-watermark.innerHTML = `<span style="text-shadow: -1px 0.5px 0 #45C6D1, -2px 0px 0 #45C6D1;">CyberHacksKhan</span> <span style="color:gray; padding-left:2px; font-family: Arial, sans-serif; font-size:10px">${ver}</span>`;
+watermark.innerHTML = `<span style="text-shadow: -1px 0.5px 0 #00FFFF, -2px 0px 0 #00FFFF;">CyberHacksKhan</span> <span style="color:gray; padding-left:2px; font-family: Arial, sans-serif; font-size:10px">${ver}</span>`;
 
 document.body.appendChild(watermark);
 
@@ -93,9 +93,9 @@ dropdownMenu.innerHTML = `
     <style>
         input[type="checkbox"] {appearance: none; width: 15px; height: 15px; background-color: #153F4D;
         border: 1px solid #acacac; border-radius: 3px; margin-right: 5px; cursor: pointer;}
-        input[type="checkbox"]:checked {background-color: #45C6D1; border-color: #45C6D1;}
+        input[type="checkbox"]:checked {background-color: #00FFFF; border-color: #00FFFF;}
         input[type="text"], input[type="number"], input[type="range"] {width: calc(100% - 10px); border: 1px solid #153F4D; 
-        color: white; accent-color: #45C6D1; background-color: #45C6D1; padding: 3px; border-radius: 3px; background: none;}
+        color: white; accent-color: #00FFFF; background-color: #00FFFF; padding: 3px; border-radius: 3px; background: none;}
         label {display: flex; align-items: center; color: #153F4D; padding-top: 3px;}
     </style>
 `;
