2c6c308eadbf65bc38ff8a0cfc9dbddc1a1ef790
Davizinzkn <117551326+davizinzknVkv@users.noreply.github.com>
2025-10-30 19:15:22 -0300

    TINHA ESQUECIDO KKK

--- functions/customBanner.js
+++ functions/customBanner.js
@@ -4,7 +4,7 @@ const phrases = [
     "[🤬] CyberHacksKhan on top.",
     "[🤬] davizinzkn said hello!",
     "[🤬] Deus, eu queria ter o CyberHacks.",
-    "[🤬] Fique bom, pegue ScrotKhan!",
+    "[🤬] Fique bom, pegue CyberHacksKhan!",
     "[🤬] cyberhackskhan.space goes brrrrr" 
 ];
 
