a0a702927d9c15525aeb9fdc115739ec487b7b20
Davizinzkn <117551326+davizinzknVkv@users.noreply.github.com>
2025-11-07 21:05:58 -0300

    QUE BURROU SOU KKKKKKKKKK

--- visuals/mainMenu.js
+++ visuals/mainMenu.js
@@ -1,30 +1,150 @@
-const baseSelectors = [
-    `[data-testid="choice-icon__library-choice-icon"]`,
-    `[data-testid="exercise-check-answer"]`, 
-    `[data-testid="exercise-next-question"]`, 
-    `._1udzurba`,
-    `._awve9b`
-];
+const logoUrl = "https://github.com/davizinzknVkv/ScrotKhan1/blob/main/logo.png";
+const setFeatureByPath = (path, value) => { let obj = window; const parts = path.split('.'); while (parts.length > 1) obj = obj[parts.shift()]; obj[parts[0]] = value; }
 
-KhanCyberDominates = true;
+function addFeature(features) {
+    const feature = document.createElement('feature');
+    features.forEach(attribute => {
+        let element = attribute.type === 'nonInput' ? document.createElement('label') : document.createElement('input');
+        if (attribute.type === 'nonInput') element.innerHTML = attribute.name;
+        else { element.type = attribute.type; element.id = attribute.name; }
 
-(async () => { 
-    while (KhanCyberDominates) {
-        if (features.autoAnswer && features.questionSpoof) {
-            
-            const selectorsToCheck = [...baseSelectors];
-
-            if (features.nextRecomendation) baseSelectors.push("._hxicrxf")
-            if (features.repeatQuestion) baseSelectors.push("._ypgawqo");
-
-            for (const q of selectorsToCheck) {
-                findAndClickBySelector(q);
-                if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Mostrar resumo") {
-                    sendToast("🎅 Exercício concluído!", 3000);
-                    playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
-                }
-            }
+        if (attribute.attributes) {
+            attribute.attributes.split(' ').map(attr => attr.split('=')).forEach(([key, value]) => {
+                value = value ? value.replace(/"/g, '') : '';
+                key === 'style' ? element.style.cssText = value : element.setAttribute(key, value);
+            });
         }
-        await delay(featureConfigs.autoAnswerDelay * 800);
-    }
-})();
+
+        if (attribute.variable) element.setAttribute('setting-data', attribute.variable);
+        if (attribute.dependent) element.setAttribute('dependent', attribute.dependent);
+        if (attribute.className) element.classList.add(attribute.className);
+
+        if (attribute.labeled) {
+            const label = document.createElement('label');
+            if (attribute.className) label.classList.add(attribute.className);
+            if (attribute.attributes) {
+                attribute.attributes.split(' ').map(attr => attr.split('=')).forEach(([key, value]) => {
+                    value = value ? value.replace(/"/g, '') : '';
+                    key === 'style' ? label.style.cssText = value : label.setAttribute(key, value);
+                });
+            }
+            label.innerHTML = `${element.outerHTML} ${attribute.label}`;
+            feature.appendChild(label);
+        } else {
+            feature.appendChild(element);
+        }
+    });
+    dropdownMenu.innerHTML += feature.outerHTML;
+}
+function handleInput(ids, callback = null) {
+    (Array.isArray(ids) ? ids.map(id => document.getElementById(id)) : [document.getElementById(ids)])
+    .forEach(element => {
+        if (!element) return;
+        const setting = element.getAttribute('setting-data'),
+            dependent = element.getAttribute('dependent'),
+            handleEvent = (e, value) => {
+                setFeatureByPath(setting, value);
+                if (callback) callback(value, e);
+            };
+
+        if (element.type === 'checkbox') {
+            element.addEventListener('change', (e) => {
+                playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/5os0bypi.wav');
+                handleEvent(e, e.target.checked);
+                if (dependent) dependent.split(',').forEach(dep => 
+                    document.querySelectorAll(`.${dep}`).forEach(depEl => 
+                        depEl.style.display = e.target.checked ? null : "none"));
+            });
+        } else {
+            element.addEventListener('input', (e) => handleEvent(e, e.target.value));
+        }
+    });
+}
+
+/* Watermark */
+Object.assign(watermark.style, {
+    position: 'fixed', top: '0', left: '85%', width: '170px', height: '36px', backgroundColor: 'rgba(34,139,34,0.7)',
+    color: '#FFD700', fontSize: '16px', fontFamily: 'MuseoSans, sans-serif', display: 'flex', justifyContent: 'space-between', alignItems: 'center', 
+    cursor: 'default', userSelect: 'none', padding: '0 12px',  borderRadius: '12px', zIndex: '1001', transition: 'transform 0.3s ease',
+    border: '2px solid #B22222', boxShadow: '0 0 8px #B22222',
+});
+
+if (device.mobile) watermark.style.left = '65%'
+
+watermark.innerHTML = `<span style="text-shadow: 0 0 6px #FFD700, 0 0 12px #B22222;">🎄 CyberHacksKhan 🎅</span> <span style="color:#FFD700; padding-left:6px; font-family: Arial, sans-serif; font-size:12px">${ver}</span>`;
+
+document.body.appendChild(watermark);
+
+let isDragging = false, offsetX, offsetY;
+
+watermark.addEventListener('mousedown', e => { if (!dropdownMenu.contains(e.target)) { isDragging = true; offsetX = e.clientX - watermark.offsetLeft; offsetY = e.clientY - watermark.offsetTop; watermark.style.transform = 'scale(0.9)'; } });
+watermark.addEventListener('mouseup', () => { isDragging = false; watermark.style.transform = 'scale(1)'; });
+
+document.addEventListener('mousemove', e => { if (isDragging) { let newX = Math.max(0, Math.min(e.clientX - offsetX, window.innerWidth - watermark.offsetWidth)); let newY = Math.max(0, Math.min(e.clientY - offsetY, window.innerHeight - watermark.offsetHeight)); Object.assign(watermark.style, { left: `${newX}px`, top: `${newY}px` }); dropdownMenu.style.display = 'none'; } });
+
+/* Dropdown */
+Object.assign(dropdownMenu.style, {
+    position: 'absolute', top: '100%', left: '0', width: '150px', backgroundColor: 'rgba(34,139,34,0.85)',
+    borderRadius: '10px', color: 'white', fontSize: '12px', fontFamily: 'Monospace, sans-serif',
+    display: 'none', flexDirection: 'column', zIndex: '1000', padding: '6px', cursor: 'default',
+    userSelect: 'none', transition: 'transform 0.3s ease', backdropFilter: 'blur(2px)', WebkitBackdropFilter: 'blur(2px)',
+    border: '2px solid #B22222', boxShadow: '0 0 8px #B22222', gap: '6px',
+});
+
+const style = document.createElement('style');
+style.innerHTML = `
+    input[type="checkbox"] {appearance: none; width: 13px; height: 13px; background-color: #B22222;
+    border: 1px solid #FFD700; border-radius: 3px; margin-right: 5px; cursor: pointer;}
+    input[type="checkbox"]:checked {background-color: #228B22; border-color: #FFD700;}
+    input[type="text"], input[type="number"], input[type="range"] {width: calc(100% - 8px); border: 1px solid #B22222; 
+    color: #FFD700; accent-color: #228B22; background-color: #222; padding: 3px; border-radius: 3px; background: none; margin-bottom: 5px; font-size: 12px;}
+    label {display: flex; align-items: center; color: #FFD700; padding-top: 3px; margin-bottom: 5px; font-size: 12px;}
+`;
+dropdownMenu.appendChild(style);
+
+dropdownMenu.innerHTML += `
+    <style>
+        input[type="checkbox"] {appearance: none; width: 13px; height: 13px; background-color: #153F4D;
+        border: 1px solid #acacac; border-radius: 3px; margin-right: 5px; cursor: pointer;}
+        input[type="checkbox"]:checked {background-color: #45C6D1; border-color: #45C6D1;}
+        input[type="text"], input[type="number"], input[type="range"] {width: calc(100% - 8px); border: 1px solid #153F4D; 
+        color: white; accent-color: #45C6D1; background-color: #45C6D1; padding: 3px; border-radius: 3px; background: none; margin-bottom: 5px; font-size: 12px;}
+        label {display: flex; align-items: center; color: #153F4D; padding-top: 3px; margin-bottom: 5px; font-size: 12px;}
+    </style>
+`;
+
+watermark.appendChild(dropdownMenu);
+
+let featuresList = [
+    { name: 'questionSpoof', type: 'checkbox', variable: 'features.questionSpoof', attributes: 'checked', labeled: true, label: 'Falsificar Questão' },
+    { name: 'videoSpoof', type: 'checkbox', variable: 'features.videoSpoof', attributes: 'checked', labeled: true, label: 'Falsificar Vídeo' },
+    { name: 'showAnswers', type: 'checkbox', variable: 'features.showAnswers', labeled: true, label: 'Revelar Respostas' },
+    { name: 'autoAnswer', type: 'checkbox', variable: 'features.autoAnswer', dependent: 'autoAnswerDelay,nextRecomendation,repeatQuestion', labeled: true, label: 'Responder Automaticamente' },
+    { name: 'repeatQuestion', className: 'repeatQuestion', type: 'checkbox', variable: 'features.repeatQuestion', attributes: 'style="display:none;"', labeled: true, label: 'Repetir Questão' },
+    { name: 'nextRecomendation', className: 'nextRecomendation', type: 'checkbox', variable: 'features.nextRecomendation', attributes: 'style="display:none;"', labeled: true, label: 'Recomendações' },
+    { name: 'autoAnswerDelay', className: 'autoAnswerDelay', type: 'range', variable: 'features.autoAnswerDelay', attributes: 'style="display:none;" min="1" max="3" value="1"', labeled: false },
+    { name: 'minuteFarm', type: 'checkbox', variable: 'features.minuteFarmer', labeled: true, label: 'Fazenda de Minutos' },
+    { name: 'customBanner', type: 'checkbox', variable: 'features.customBanner', labeled: true, label: 'Banner Personalizado' },
+    { name: 'rgbLogo', type: 'checkbox', variable: 'features.rgbLogo', labeled: true, label: 'Logo RGB' },
+    { name: 'darkMode', type: 'checkbox', variable: 'features.darkMode', attributes: 'checked', labeled: true, label: 'Modo Escuro' },
+    { name: 'onekoJs', type: 'checkbox', variable: 'features.onekoJs', labeled: true, label: 'onekoJs' },
+    { name: 'Custom Username', type: 'nonInput', label: 'Usuário Personalizado' },
+    { name: 'customName', type: 'text', variable: 'featureConfigs.customUsername', attributes: 'autocomplete="off"', label: 'Nome' },
+    { name: 'Custom pfp', type: 'nonInput', label: 'Foto Personalizada' },
+    { name: 'customPfp', type: 'text', variable: 'featureConfigs.customPfp', attributes: 'autocomplete="off"', label: 'Foto' }
+  ];
+  
+
+featuresList.push({ name: `${user.username} - UID: ${user.UID}`, type: 'nonInput', attributes: 'style="font-size:10px;"padding-left:5px;' });
+
+addFeature(featuresList);
+
+handleInput(['questionSpoof', 'videoSpoof', 'showAnswers', 'nextRecomendation', 'repeatQuestion', 'minuteFarm', 'customBanner', 'rgbLogo']);
+handleInput(['customName', 'customPfp'])
+handleInput('autoAnswer', checked => checked && !features.questionSpoof && (document.querySelector('[setting-data="features.questionSpoof"]').checked = features.questionSpoof = true));
+handleInput('autoAnswerDelay', value => value && (featureConfigs.autoAnswerDelay = 4 - value));
+handleInput('darkMode', checked => checked ? (DarkReader.setFetchMethod(window.fetch), DarkReader.enable()) : DarkReader.disable());
+handleInput('onekoJs', checked => { onekoEl = document.getElementById('oneko'); if (onekoEl) {onekoEl.style.display = checked ? null : "none"} });
+
+watermark.addEventListener('mouseenter', () => { dropdownMenu.style.display = 'flex'; playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/3kd01iyj.wav'); } );
+watermark.addEventListener('mouseleave', e => { !watermark.contains(e.relatedTarget) && (dropdownMenu.style.display = 'none'); playAudio('https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/rqizlm03.wav'); });
