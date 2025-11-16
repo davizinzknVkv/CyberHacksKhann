619b4e6f4b421ffa0ffc1e6f04615318ab52246b
Davizinzkn <117551326+davizinzknVkv@users.noreply.github.com>
2025-11-15 23:43:08 -0300

    teste

--- functions/autoAnswer.js
+++ functions/autoAnswer.js
@@ -1,26 +1,25 @@
 const baseSelectors = [
-    `[data-testid="choice-icon__library-choice-icon"]`,
+    `.perseus_hm3uu-sq`,
     `[data-testid="exercise-check-answer"]`, 
     `[data-testid="exercise-next-question"]`, 
-    `._1udzurba`,
-    `._awve9b`
+    `._1wi2tma4`
 ];
 
-KhanCyberDominates = true;
+khanwareDominates = true;
 
 (async () => { 
-    while (KhanCyberDominates) {
+    while (khanwareDominates) {
         if (features.autoAnswer && features.questionSpoof) {
             
             const selectorsToCheck = [...baseSelectors];
 
-            if (features.nextRecomendation) baseSelectors.push("._hxicrxf")
-            if (features.repeatQuestion) baseSelectors.push("._ypgawqo");
+            if (features.nextRecomendation) selectorsToCheck.push("._g9riz5o")
+            if (features.repeatQuestion) selectorsToCheck.push("._10goqnzn");
 
             for (const q of selectorsToCheck) {
                 findAndClickBySelector(q);
                 if (document.querySelector(q+"> div") && document.querySelector(q+"> div").innerText === "Mostrar resumo") {
-                    sendToast("🎅 Exercício concluído!", 3000);
+                    sendToast("🎉 Exercício concluído!", 3000);
                     playAudio("https://r2.e-z.host/4d0a0bea-60f8-44d6-9e74-3032a64a9f32/4x5g14gj.wav");
                 }
             }
